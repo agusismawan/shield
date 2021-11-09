21ea324b2fd21c0437f3ffb155f8c9251139fdd7
Afif Muhammad <afif.rizqiawan@gmail.com>
2021-11-09 21:26:43 +0700

    Add login & session management

---
+++ pages/auth.jsx
@@ -0,0 +1,113 @@
+import Head from "next/head";
+import router from "next/router";
+import { useForm } from "react-hook-form";
+import fetchJson from "../lib/fetchJson";
+import { useState } from "react";
+
+export default function Login() {
+  const { register, handleSubmit } = useForm();
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetchJson("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      if (response.status == 200) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error("An unexpected error happened:", error);
+      setErrorMsg(error.data.message);
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Incident & Problem Management</title>
+      </Head>
+      <div className="min-h-screen bg-white flex">
+        <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+              <img
+                className="h-12 w-auto"
+                src="/shield-logo.png"
+                alt="Workflow"
+              />
+              <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+                Sign in to your account
+              </h2>
+            </div>
+
+            <div className="mt-8">
+              <div className="mt-6">
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                  <div>
+                    {errorMsg && (
+                      <p className="mb-1 text-sm text-red-600">{errorMsg}</p>
+                    )}
+                    <label
+                      htmlFor="username"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Username
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="username"
+                        {...register("username", { required: true })}
+                        type="text"
+                        autoComplete="username"
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="space-y-1">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="password"
+                        {...register("password", { required: true })}
+                        type="password"
+                        autoComplete="password"
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:block relative w-0 flex-1">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,33 @@
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 import Head from "next/head";
+import withSession from "../lib/session";
 
-function Home() {
+export const getServerSideProps = withSession(async function ({ req, res }) {
+  const user = req.session.get("user");
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { user: req.session.get("user") },
+  };
+});
+
+function Home({ user }) {
+  console.log(user);
   return (
     <>
       <Layout>
         <Head>
-          <title>Incident Report</title>
+          <title>Incident & Problem Management</title>
         </Head>
+        <h1>{user.accessToken}</h1>
       </Layout>
     </>
   );
