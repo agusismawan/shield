817cde2ec4281b3710aff76a537f82846904432a
Afif Muhammad <afif.rizqiawan@gmail.com>
2021-11-10 12:01:30 +0700

    Remove activity timeline

--- pages/incidents/[id].jsx
+++ pages/incidents/[id].jsx
@@ -518,52 +518,6 @@ function IncidentDetail({ incident, type, urgency, impact }) {
                     </div>
                   </section>
                 )}
-
-                <section aria-labelledby="activity-timeline">
-                  <div className="flex flex-col">
-                    <div className="mt-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                      <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                          <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
-                            <h3 className="text-lg leading-6 font-medium text-gray-900">
-                              Timeline
-                            </h3>
-                          </div>
-                          <table className="min-w-full divide-y divide-gray-200">
-                            <thead className="bg-gray-50">
-                              <tr>
-                                <th
-                                  scope="col"
-                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                  Time
-                                </th>
-                                <th
-                                  scope="col"
-                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                  Activity
-                                </th>
-                              </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-200">
-                              {people.map((person) => (
-                                <tr key={person.email}>
-                                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                    {person.name}
-                                  </td>
-                                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {person.title}
-                                  </td>
-                                </tr>
-                              ))}
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </section>
               </div>
 
               <section
