b6a3227b8cc3327b3ba03234270572b800c7f922
Afif Muhammad <afif.rizqiawan@gmail.com>
2022-01-05 14:07:18 +0700

    Update favicon

--- components/meta.jsx
+++ components/meta.jsx
@@ -10,7 +10,7 @@ export default function Meta() {
       />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="robots" content="noindex,nofollow" />
-      <link rel="shortcut icon" href="/shield-favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
   );
 }
