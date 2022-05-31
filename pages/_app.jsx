14262cc7e45597e93f39c9e86e3e4b21cb32718c
Agus Dwi Ismawan <agusdwiismawan@gmail.com>
2022-05-31 17:05:44 +0700

    ft. rollback antd

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,9 @@
-import "../styles/antd.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import NProgress from "nprogress";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
+import "../styles/antd.css";
 import "nprogress/nprogress.css";
 import "react-toastify/dist/ReactToastify.css";
 
