7bfc7101b1bbb7234f9d089d145fb29e87b89228
haritsf <haritsf@hotmail.com>
2022-07-05 11:51:07 +0700

    Fixing AlwaysOpen at Sub-Sidebar, Separate Disclosure into Utils

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import { classNames } from "./utils";
+import { classNames, DisclosureDefault, DisclosureOpen } from "./utils";
 import { Fragment } from "react";
-import { Menu, Transition, Disclosure } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 import { SelectorIcon, UserCircleIcon } from "@heroicons/react/solid";
-import { ChevronRightIcon } from "@heroicons/react/outline";
-import { checkMemberAES } from "./problems/ProblemHelper";
 
 export default function Sidebar({ navigation, router, session, action }) {
   return (
@@ -150,85 +148,21 @@ export default function Sidebar({ navigation, router, session, action }) {
                       </a>
                     </Link>
                   ) : (
-                    <Disclosure as="div" key={item.name} className="space-y-1">
-                      {({ open }) => (
-                        <>
-                          <Disclosure.Button
-                            className={classNames(
-                              router.pathname.includes(item.href)
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-600 hover:text-gray-900 hover:bg-gray-50",
-                              "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            <item.icon
-                              className={classNames(
-                                router.pathname.includes(item.href)
-                                  ? "text-gray-500"
-                                  : "text-gray-400 group-hover:text-gray-500",
-                                "mr-3 flex-shrink-0 h-6 w-6"
-                              )}
-                              aria-hidden="true"
-                            />
-                            {item.name}
-                            <ChevronRightIcon
-                              className={classNames(
-                                open
-                                  ? "text-gray-400 rotate-90 transition-transform ease-in-out duration-250"
-                                  : "text-gray-300 transition-transform ease-in-out duration-250",
-                                "ml-2 flex-shrink-0 h-5 w-5 transform group-hover:text-gray-400 transition-colors ease-in-out duration-250"
-                              )}
-                              aria-hidden="true"
-                            />
-                          </Disclosure.Button>
-                          <Disclosure.Panel className="space-y-1">
-                            {checkMemberAES(session)
-                              ? item.children.map((subItem) => (
-                                  <Link key={subItem.name} href={subItem.href}>
-                                    <a
-                                      className={classNames(
-                                        router.pathname == subItem.href
-                                          ? "bg-gray-100 text-gray-900"
-                                          : "text-gray-600 hover:text-gray-900 hover:bg-gray-50",
-                                        "group flex items-center pl-12 pr-2 py-2 text-sm font-medium rounded-md"
-                                      )}
-                                      aria-current={
-                                        subItem.current ? "page" : undefined
-                                      }
-                                    >
-                                      {subItem.name}
-                                    </a>
-                                  </Link>
-                                ))
-                              : item.children
-                                  .filter((check) => check.permission == "all")
-                                  .map((subItem) => {
-                                    return (
-                                      <Link
-                                        key={subItem.name}
-                                        href={subItem.href}
-                                      >
-                                        <a
-                                          className={classNames(
-                                            router.pathname == subItem.href
-                                              ? "bg-gray-100 text-gray-900"
-                                              : "text-gray-600 hover:text-gray-900 hover:bg-gray-50",
-                                            "group flex items-center pl-12 pr-2 py-2 text-sm font-medium rounded-md"
-                                          )}
-                                          aria-current={
-                                            subItem.current ? "page" : undefined
-                                          }
-                                        >
-                                          {subItem.name}
-                                        </a>
-                                      </Link>
-                                    );
-                                  })}
-                          </Disclosure.Panel>
-                        </>
+                    <>
+                      {router.pathname.includes(item.href) ? (
+                        <DisclosureOpen
+                          item={item}
+                          router={router}
+                          session={session}
+                        />
+                      ) : (
+                        <DisclosureDefault
+                          item={item}
+                          router={router}
+                          session={session}
+                        />
                       )}
-                    </Disclosure>
+                    </>
                   )
                 )}
               </div>
